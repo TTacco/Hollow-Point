2a382380bf28386a9ab1c265b989ee06d910c97e
TTacco <joventadem@gmail.com>
2018-08-29 20:28:20 +0800

    Add project files.

---
+++ HollowPoint/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HollowPoint
+{
+    public class Class1
+    {
+    }
+}
